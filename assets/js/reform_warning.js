e95ce76501d4628b1739ed7005be2db867a94bb8
Neutrino <1784986256@qq.com>
2019-03-23 15:25:27 +0800

    remove alert in reform_warning.js

--- assets/js/reform_warning.js
+++ assets/js/reform_warning.js
@@ -1,7 +1,7 @@
 var reform_alert_content, reform_warning_content;
 reform_alert_content = "为了规范使用各实验室，系实验室管理委员会对使用实验室的所有学生（包括本科生和研究生）进行规范操作，详细内容请前往网上预约平台-文档下载中心下载相关文档进行浏览或直接浏览本指南中的相关通知。由于本网站更新不一定及时，如发现校内的网上预约平台上的任何内容和实验室指南的任何内容发生冲突，请以网上预约平台上的内容为准。";
 reform_warning_content = "为了规范使用各实验室，系实验室管理委员会对使用实验室的所有学生（包括本科生和研究生）进行规范操作，详细内容请前往<a href=\"http://222.200.170.55:8081/Article/ShowList?ArticleCategoryId=8af1dbc8-9540-4471-b234-4586228e5c09&amp;XPath=000\">网上预约平台-文档下载中心</a>下载相关文档进行浏览或直接浏览<strong><a href=\"https://neutrino3316.github.io/balyspusys/docs/lab_reform/\">本指南中的相关通知</a></strong>。由于本网站更新不一定及时，如发现校内的网上预约平台上的任何内容和实验室指南的任何内容发生冲突，请以网上预约平台上的内容为准。"
-window.alert(reform_alert_content);
+// window.alert(reform_alert_content);
 // document.getElementById("warning_heading").innerHTML = "提醒标题已修改。";
 document.getElementById("warning_content").innerHTML = reform_warning_content;
 
