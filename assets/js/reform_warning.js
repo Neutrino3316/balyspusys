1dbdc50a36f1a18c8376b74c15a80b548f9c625e
Neutrino <1784986256@qq.com>
2019-03-23 14:10:50 +0800

    Update reform_warning.js

--- assets/js/reform_warning.js
+++ assets/js/reform_warning.js
@@ -1,7 +1,8 @@
-var reform_warning_content;
-reform_warning_content = "为了规范使用各实验室，系实验室管理委员会对使用实验室的所有学生（包括本科生和研究生）进行规范操作，详细内容请前往网上预约平台-文档下载中心下载相关文档进行浏览。由于本网站更新不一定及时，如发现校内的网上预约平台上的任何内容和实验室指南的任何内容发生冲突，请以网上预约平台上的内容为准。";
-// window.alert(reform_warning_content);
-document.getElementById("warning_heading").innerHTML = "提醒标题已修改。";
-document.getElementById("warning_content").innerHTML = "提醒内容已修改。";
+var reform_alert_content, reform_warning_content;
+reform_alert_content = "为了规范使用各实验室，系实验室管理委员会对使用实验室的所有学生（包括本科生和研究生）进行规范操作，详细内容请前往网上预约平台-文档下载中心下载相关文档进行浏览。由于本网站更新不一定及时，如发现校内的网上预约平台上的任何内容和实验室指南的任何内容发生冲突，请以网上预约平台上的内容为准。";
+reform_warning_content = "为了规范使用各实验室，系实验室管理委员会对使用实验室的所有学生（包括本科生和研究生）进行规范操作，详细内容请前往<a href=\"http://222.200.170.55:8081/Article/ShowList?ArticleCategoryId=8af1dbc8-9540-4471-b234-4586228e5c09&amp;XPath=000\">网上预约平台-文档下载中心</a>下载相关文档进行浏览。由于本网站更新不一定及时，如发现校内的<a href=\"http://222.200.170.55:8081\">网上预约平台</a>上的任何内容和<a href=\"https://neutrino3316.github.io/balyspusys/\">实验室指南</a>的任何内容发生冲突，请以<a href=\"http://222.200.170.55:8081\">网上预约平台</a>上的内容为准。"
+window.alert(reform_alert_content);
+// document.getElementById("warning_heading").innerHTML = "提醒标题已修改。";
+document.getElementById("warning_content").innerHTML = reform_warning_content;
 
 // <p>为了规范使用各实验室，系实验室管理委员会对使用实验室的所有学生（包括本科生和研究生）进行规范操作，详细内容请前往<a href="http://222.200.170.55:8081/Article/ShowList?ArticleCategoryId=8af1dbc8-9540-4471-b234-4586228e5c09&amp;XPath=000">网上预约平台-文档下载中心</a>下载相关文档进行浏览。由于本网站更新不一定及时，如发现校内的<a href="http://222.200.170.55:8081">网上预约平台</a>上的任何内容和<a href="https://neutrino3316.github.io/balyspusys/">实验室指南</a>的任何内容发生冲突，请以<a href="http://222.200.170.55:8081">网上预约平台</a>上的内容为准。</p>
