a247b390ac58dddcb031c2d4bd306969a3d06adb
Neutrino <1784986256@qq.com>
2019-03-23 13:49:25 +0800

    add document.getElementById(id)

--- assets/js/reform_warning.js
+++ assets/js/reform_warning.js
@@ -1,3 +1,4 @@
 var reform_warning_content;
 reform_warning_content = "为了规范使用各实验室，系实验室管理委员会对使用实验室的所有学生（包括本科生和研究生）进行规范操作，详细内容请前往网上预约平台-文档下载中心下载相关文档进行浏览。由于本网站更新不一定及时，如发现校内的网上预约平台上的任何内容和实验室指南的任何内容发生冲突，请以网上预约平台上的内容为准。";
 window.alert(reform_warning_content);
+document.getElementById("reform_warning_content").innerHTML = "段落已修改。";
