d07cfe884676e8109677fb4d473547750f09e85b
Neutrino <1784986256@qq.com>
2019-06-19 20:27:35 +0800

    fix #8

--- assets/js/jump.js
+++ assets/js/jump.js
@@ -1,6 +1,8 @@
-// console.log(location)
-
-if (location.host != '172.18.35.126')
+// if local network host is available then jump to local network host
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "http://172.18.35.126/balyspusys/");
+xhr.send();
+if (xhr.status == 200)
 {
     location = 'http://172.18.35.126' + location.pathname
 }
